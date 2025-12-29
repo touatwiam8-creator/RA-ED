4a3f013de4a13eed9281c3df1e8258232f397291
touatwiam8-creator <touatwiam8@gmail.com>
2025-12-29 07:08:07 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,17 @@
+export default {
+  async fetch(request, env) {
+    if (request.method !== "POST") {
+      return new Response("Only POST allowed", { status: 405 });
+    }
 
+    const body = await request.json();
+
+    return new Response(
+      JSON.stringify({
+        ok: true,
+        received: body
+      }),
+      { headers: { "Content-Type": "application/json" } }
+    );
+  }
+};
